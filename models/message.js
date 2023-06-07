f4d902dc8df1c738975bf31bae166bffcbaee968
Josh S <aaabccc87@protonmail.com>
2023-06-06 22:50:21 -0400

    Create models for users and messages

---
+++ models/message.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now(),
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 1,
+    maxLength: 50,
+  },
+  body: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 1,
+    maxLength: 500,
+  }
+});
+
+module.exports = mongoose.model('Message', messageSchema);
---
+++ models/user.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 1,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 1,
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 1,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    validate: {
+      validator: validator.isEmail,
+      message: 'Invalid email address encountered during user validation',
+    }
+  },
+  salt: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 32,
+  },
+  hash: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 64,
+  },
+  memberStatus: {
+    type: Boolean,
+    required: true,
+  },
+  isAdmin: Boolean,
+});
+
+module.exports = mongoose.model('User', userSchema);
