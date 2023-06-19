98512604905933ba301cf2dea11c26e1db8141a3
Josh S <aaabccc87@protonmail.com>
2023-06-19 15:45:59 -0400

    Routes and mock controllers for maintaining profile information.
    Note: controller to get view for updating personal info is complete and
      links are added to user_detail view if a user views their own page

--- controllers/userController.js
+++ controllers/userController.js
@@ -48,3 +48,63 @@ exports.get_user_details = asyncHandler(async (req, res, next) => {
   });
 
 });
+
+exports.get_update_info = asyncHandler(async(req, res, next) => {
+  if (req.isAuthenticated()) {
+    if (req.user.username !== req.params.username) {
+      const err = createError(403, 'Forbidden');
+      return next(err);
+    }
+  } else {
+    const err = createError(401, 'Unauthorized');
+    return next(err);
+  }
+
+  const user = await User.findOne({ username: req.params.username }).exec();
+
+  if (user === null) {
+    const err = createError(404, 'User not found');
+    return next(err);
+  }
+
+  res.render('update_personal_info', {
+    title: 'Update Personal Info',
+    user: req.user,
+  });
+});
+
+exports.post_update_info = (req, res, next) => {
+  res.send("POST UPDATE INFO: Not Implemented");
+}
+
+exports.get_change_password = (req, res, next) => {
+  res.send('GET CHANGE PASSWORD: Not implemented');
+};
+
+exports.post_change_password = (req, res, next) => {
+  res.send('POST CHANGE PASSWORD: Not implemented');
+};
+
+exports.get_account_settings = (req, res, next) => {
+  res.send('GET ACCOUNT SETTINGS: Not implemented');
+};
+
+exports.post_account_settings = (req, res, next) => {
+  res.send('POST ACCOUNT SETTINGS: Not implemented');
+};
+
+exports.get_membership_status = (req, res, next) => {
+  res.send('GET MEMBERSHIP STATUS: Not implemented');
+};
+
+exports.post_membership_status = (req, res, next) => {
+  res.send('POST MEMBERSHIP STATUS: Not implemented');
+};
+
+exports.get_delete_account = (req, res, next) => {
+  res.send('GET DELETE ACCOUNT: Not implemented');
+};
+
+exports.post_delete_account = (req, res, next) => {
+  res.send('POST DELETE ACCOUNT: Not implemented');
+};
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -3,6 +3,21 @@ const userController = require('../controllers/userController.js');
 
 const userRouter = express.Router();
 
+userRouter.get('/:username/update-info', userController.get_update_info);
+userRouter.post('/:username/update-info', userController.post_update_info);
+
+userRouter.get('/:username/change-password', userController.get_change_password);
+userRouter.post('/:username/change-password', userController.post_change_password);
+
+userRouter.get('/:username/account-settings', userController.get_account_settings);
+userRouter.post('/:username/account-settings', userController.post_account_settings);
+
+userRouter.get('/:username/membership-status', userController.get_membership_status);
+userRouter.post('/:username/membership-status', userController.post_membership_status);
+
+userRouter.get('/:username/delete-account', userController.get_delete_account);
+userRouter.post('/:username/delete-account', userController.post_delete_account);
+
 userRouter.use('/:username', userController.get_user_details);
 
 module.exports = userRouter;
